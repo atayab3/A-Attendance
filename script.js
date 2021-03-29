a9106534b42675f6f0951b67a9c0eb77d49b7413
mmariy2 <64489700+mmariy2@users.noreply.github.com>
2021-03-29 16:38:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -121,20 +121,6 @@ function health(){
 
 }
 
-//when quarantine buttons are clicked
-var count = 1;
-function setColor(btn, color) {
-  var property = document.getElementById(btn);
-  if (count == 0) {
-      property.style.backgroundColor = "#FFFFFF"
-            count = 1;        
-  }
-  else {
-    property.style.backgroundColor = "#7FFF00"
-    count = 0;
-  }
-}
-
 // -- DIRECTORY PAGE -- 
 //Transition to directory page from homePage
 homePage.querySelector('.directButton').addEventListener("click", directory);
